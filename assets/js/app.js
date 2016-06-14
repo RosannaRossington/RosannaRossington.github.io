c7165f20eaadfdc588b08070941331f314c140e4
RosannaRossington <rosanna_rossington@outlook.com>
2016-06-14 17:26:59 +0100

    showing and hiding tech

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,37 @@
 console.log ("hello");
+$(start);
+
+function start(){
+  technologiesPage();
+  setupEvents();
+}
+
+function technologiesPage(){
+  return $.each([
+    "#confident",
+    "#learning",
+    "#intrigued"
+  ], function(element, value){
+    $(value).hide();
+  });
+}
+
+function setupEvents(){
+  var $confident      = $("#confident");
+  // var $learning       = $("#learning");
+  // var $intrigued      = $("#intrigued");
+
+  $confident.on("click", showConfident);
+  // $learning.on("click", showLearning);
+  // $intrigued.on("click", showIntrigued);
+}
+
+function showConfident(){
+  var $confident      = $("#confident");
+  var $confidentTech  = $("#confidentTech");
+
+  $(this).hide();
+  $confident.fadeOut("500", function(){
+    $confidentTech.show();
+  });
+}
