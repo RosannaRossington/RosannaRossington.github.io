0b75b1511f343e4cb760cca56c7752e195edf9fc
RosannaRossington <rosanna_rossington@outlook.com>
2016-07-26 22:13:19 +0100

    removing home

---
+++ assets/js/app.js
@@ -0,0 +1,128 @@
+console.log("yo");
+  $(start);
+
+function start(){
+  technologiesPage();
+  setupEvents();
+}
+
+function technologiesPage(){
+  return $.each([
+    "#understanding",
+    "#gaIntro",
+    "#intriguedTitle",
+    "#confidentTitle",
+    "#learningTitle"
+   ], function(element, value){
+    $(value).show();
+  });
+}
+
+function setupEvents(){
+  var $confident      = $("#confident");
+  var $learning       = $("#learning");
+  var $intrigued      = $("#intrigued");
+
+  var $confidentTech  = $("#confidentTech");
+  var $learningTech   = $("#learningTech");
+  var $intriguedTech  = $("#intriguedTech");
+
+  var $intriguedTitle = $("#intriguedTitle");
+  var $confidentTitle = $("#confidentTitle");
+  var $learningTitle  = $("#learningTitle");
+
+  $confident.on("click", showConfident);
+  $learning.on("click", showLearning);
+  $intrigued.on("click", showIntrigued);
+
+  $confidentTech.hide();
+  $learningTech.hide();
+  $intriguedTech.hide();
+
+  $intriguedTitle.hide();
+  $confidentTitle.hide();
+  $learningTitle.hide();
+}
+
+function showConfident(){
+  var $confident      = $("#confident");
+  var $learning       = $("#learning");
+  var $intrigued      = $("#intrigued");
+  var $understanding  = $("#understanding");
+
+  var $confidentTech  = $("#confidentTech");
+  var $learningTech   = $("#learningTech");
+  var $intriguedTech  = $("#intriguedTech");
+
+  var $intriguedTitle = $("#intriguedTitle");
+  var $confidentTitle = $("#confidentTitle");
+  var $learningTitle  = $("#learningTitle");
+
+  $(this).hide();
+  $confident.fadeOut("100", function(){
+    $learningTech.hide();
+    $intriguedTech.hide();
+    $confidentTech.show();
+
+    $learning.show();
+    $intrigued.show();
+
+    $intriguedTitle.hide();
+    $confidentTitle.show();
+    $learningTitle.hide();
+  });
+}
+
+function showLearning(){
+  var $confident      = $("#confident");
+  var $learning       = $("#learning");
+  var $intrigued      = $("#intrigued");
+
+  var $understanding  = $("#understanding");
+  var $confidentTech  = $("#confidentTech");
+  var $learningTech   = $("#learningTech");
+  var $intriguedTech  = $("#intriguedTech");
+
+  var $intriguedTitle = $("#intriguedTitle");
+  var $confidentTitle = $("#confidentTitle");
+  var $learningTitle  = $("#learningTitle");
+
+  $(this).hide();
+  $learning.fadeOut("100", function(){
+    $learningTech.show();
+    $intriguedTech.hide();
+    $confidentTech.hide();
+    $confident.show();
+    $intrigued.show();
+    $intriguedTitle.hide();
+    $confidentTitle.hide();
+    $learningTitle.show();
+  });
+}
+
+function showIntrigued(){
+  var $confident      = $("#confident");
+  var $learning       = $("#learning");
+  var $intrigued      = $("#intrigued");
+  var $understanding  = $("#understanding");
+
+  var $confidentTech  = $("#confidentTech");
+  var $learningTech   = $("#learningTech");
+  var $intriguedTech  = $("#intriguedTech");
+
+  var $intriguedTitle = $("#intriguedTitle");
+  var $confidentTitle = $("#confidentTitle");
+  var $learningTitle  = $("#learningTitle");
+
+  $(this).hide();
+  $intrigued.fadeOut("500", function(){
+    $learningTech.hide();
+    $intriguedTech.show();
+    $confidentTech.hide();
+    $confident.show();
+    $learning.show();
+    $intriguedTitle.show();
+    $confidentTitle.hide();
+    $learningTitle.hide();
+  });
+}
